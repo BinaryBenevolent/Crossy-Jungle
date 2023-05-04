2488f026b12691c382a4efc149513d0472534f85
kevin <73090726+BinaryBenevolent@users.noreply.github.com>
2023-05-04 10:22:46 +0700

    Limiting Road Terrain Spawn
    Limit the spawn rate of Road Terrain to max 3 terrain of roads

--- Assets/Scripts/PlayManager.cs
+++ Assets/Scripts/PlayManager.cs
@@ -4,46 +4,115 @@ using UnityEngine;
 
 public class PlayManager : MonoBehaviour
 {
+
+    [SerializeField] List<Terrain> terrainList;  
+
     [SerializeField] Grass grassPrefab;
     [SerializeField] Road roadPrefab;
 
     [SerializeField] int initialGrassCount = 5;
     [SerializeField] int horizontalSize;
 
-    [SerializeField] int backRelativePos = -4;
+    [SerializeField] int backViewDistance = -4;
+    [SerializeField] int forwardViewDistance = 15; 
 
     [SerializeField, Range(0,1)] float treeProbability;
 
+    Dictionary<int, Terrain> activeTerrainDict = new Dictionary<int, Terrain>(20);
+
     private void Start()
     {
-        for (int zPos = backRelativePos; zPos < initialGrassCount; zPos++)
+        // Create Initial Grass
+        for (int zPos = backViewDistance; zPos < initialGrassCount; zPos++)
         {
-            var grass = Instantiate(grassPrefab);
+            var terrain = Instantiate(terrainList[0]);
 
-            grass.transform.position = new Vector3(0, 0, zPos);
+            terrain.transform.position = new Vector3(0, 0, zPos);
 
-            float treeProbability;
-            if(zPos < -3)
+            if(terrain is Grass grass)
             {
-                treeProbability = 1;
+                float treeProbability;
+                if (zPos < -3)
+                {
+                    treeProbability = 1;
+                }
+                else if (zPos < -2)
+                {
+                    treeProbability = 0.5f;
+                }
+                else if (zPos < -1)
+                {
+                    treeProbability = 0.3f;
+                }
+                else
+                {
+                    treeProbability = 0;
+                }
+
+                grass.SetTreePercentage(treeProbability);
             }
-            else if(zPos < -2)
+            terrain.Generate(horizontalSize);
+
+            activeTerrainDict[zPos] = terrain;
+        }
+
+        for (int zPos = initialGrassCount; zPos < forwardViewDistance; zPos++)
+        {
+            var terrain = SpawnRandomTerrain(zPos);
+
+            terrain.Generate(horizontalSize);
+
+            activeTerrainDict[zPos] = terrain;
+        }
+
+        SpawnRandomTerrain(0);
+    }
+
+    private Terrain SpawnRandomTerrain(int zPos)
+    {
+        Terrain terrainCheck = null;
+
+        int randomIndex;
+        Terrain terrain = null;
+    
+        for(int z = -1; z >= -3; z--)
+        {
+            var checkPos = zPos + z;
+
+            if(terrainCheck == null)
             {
-                treeProbability = 0.5f;
+                terrainCheck = activeTerrainDict[checkPos];
+                continue;
             }
-            else if( zPos < -1)
+            else if (terrainCheck.GetType() != activeTerrainDict[checkPos].GetType())
             {
-                treeProbability = 0.3f;
+                randomIndex = Random.Range(0, terrainList.Count);
+                terrain = Instantiate(terrainList[randomIndex]);
+                terrain.transform.position = new Vector3(0, 0, zPos);
+
+                return terrain;
             }
             else
             {
-                treeProbability = 0;
+                continue;
             }
-
-
-            grass.SetTreePercentage(treeProbability);
-
-            grass.Generate(horizontalSize);
         }
+
+        var candidateTerrain = new List<Terrain>(terrainList);
+
+        for (int i = 0; i < candidateTerrain.Count; i++)
+        {
+            if(terrainCheck.GetType() == candidateTerrain[i].GetType())
+            {
+                candidateTerrain.Remove(candidateTerrain[i]);
+                break;
+            }
+        }
+
+        randomIndex = Random.Range(0, candidateTerrain.Count);
+        terrain = Instantiate(candidateTerrain[randomIndex]);
+        terrain.transform.position = new Vector3(0, 0, zPos);
+
+        return terrain;
     }
 }
